ca120b960bf10da64e47aa9490ceed12bb3f99ad
= <=>
2022-11-10 14:51:12 +0700

    belajar laravel blade if for

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+//URL Index / Home
+Route::get('/', function () {
+    $user = ['name' => 'David', 'role' => 'teller'];
+    return view('pages.home', $user);
+});
+
+Route::redirect('/home', '/');
+
+Route::get('/about', function(){
+    $user = ['name' => 'Asep', 'role' => 'admin'];
+    return view('pages.about', $user);
+});
+
+Route::get('/contact', function(){
+    $data = [
+        "person" => ["Rizky", "Gavin", "Firdaus", "Dzaky", "Iqbal"]
+    ];
+    return view('pages.contact', $data);
+});
