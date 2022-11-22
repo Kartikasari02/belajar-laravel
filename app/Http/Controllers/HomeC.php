b167189d7764944116634cbb3abea927107c9f39
= <=>
2022-11-23 04:31:49 +0700

    belajar laravel model & table

--- app/Http/Controllers/HomeC.php
+++ app/Http/Controllers/HomeC.php
@@ -3,8 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\StudentsM;
 
 class HomeC extends Controller
 {
-    //
+    Public function index(){
+        $user = ['name' => 'Fariz', 'role' => 'teller'];
+        return view('pages.home', $user);
+    }
+    Public function about(){
+        $user = ['name' => 'Asep', 'role' => 'admin'];
+        return view('pages.about', $user);
+    }
+    public function contact(){
+        $data = StudentsM::all();
+        return view('pages.contact', ['data' => $data]);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,36 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeC;
-use App\Http\Controllers\About;
-use App\Http\Controllers\Contact;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-//URL About
-Route::get('about', function () {
-    return 'Ini Halaman About';
-});
-
-//URL Index / Home
-Route::get('/', function () {
-    $user = ['name' => 'David', 'role' => 'teller'];
-    return view('pages.home', $user);
-});
-
+//URL Index
+Route::redirect('/', 'home');
 Route::get('/home',[HomeC::class, 'index']);
-Route::get('/home',[HomeC::class, 'About']);
-Route::get('/home',[HomeC::class, 'Contact']);
-
-Route::get('/about', function(){
-    $user = ['name' => 'Asep', 'role' => 'admin'];
-    return view('pages.about', $user);
-});
-
-Route::get('/contact', function(){
-    $data = [
-        "person" => ["Rizky", "Gavin", "Firdaus", "Dzaky", "Iqbal"]
-    ];
-    return view('pages.contact', $data);
-});
+Route::get('/about',[HomeC::class, 'about']);
+Route::get('/contact',[HomeC::class, 'contact']);
